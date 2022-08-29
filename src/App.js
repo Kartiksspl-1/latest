932d96ec1f3331fa75a84c76ee971398ca83c4b5
Ravinder <sspl.ravinder2021@gmail.com>
2022-08-29 15:12:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react'
+import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
+
 
 function App() {
+
+  useEffect(()=>{
+   
+   async function myfunc(){
+    const model = handPoseDetection.SupportedModels.MediaPipeHands;
+    const detectorConfig = {
+      runtime: 'tfjs',
+      modelType: 'full'
+    };
+   let detector = await handPoseDetection.createDetector(model, detectorConfig);
+
+
+   }
+   async function videoget(){
+
+    try{
+     const stream=await navigator.mediaDevices.getUserMedia({video:true});
+      document.getElementById('myvideo').srcObject=stream
+
+
+    }
+    catch(e){
+      console.log(e)
+    }
+   }
+   videoget()
+   myfunc()
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <video  width="250">
+
+
+
+Sorry, your browser doesn't support embedded videos.
+</video>
     </div>
   );
 }
